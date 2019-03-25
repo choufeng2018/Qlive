3088937d2174719e54862c4b3cd771431469ada7
xpwsg <xpwsgg@163.com>
2019-03-25 15:02:11 +0800

    添加直播首页控制器和页面demo

--- apps/common/controller/Widget.php
+++ apps/common/controller/Widget.php
@@ -1,7 +1,7 @@
 <?php
-// Widget基类，仅限模块的widget调用       
+// Widget基类，仅限模块的widget调用
 // +----------------------------------------------------------------------
-// | Copyright (c) 2017-2018 https://www.eacoophp.com, All rights reserved.         
+// | Copyright (c) 2017-2018 https://www.eacoophp.com, All rights reserved.
 // +----------------------------------------------------------------------
 // | [EacooPHP] 并不是自由软件,可免费使用,未经许可不能去掉EacooPHP相关版权。
 // | 禁止在EacooPHP整体或任何部分基础上发展任何派生、修改或第三方版本用于重新分发
@@ -10,51 +10,57 @@
 // +----------------------------------------------------------------------
 namespace app\common\controller;
 
-class Widget extends Base {
+class Widget extends Base
+{
 
-	public function _initialize() {
+    public function _initialize()
+    {
         parent::_initialize();
     }
 
+
     /**
-     * widget模版输出
-     * @param  string $templateFile 模板文件名
-     * @param  array  $vars         模板输出变量
-     * @param  array  $replace      模板替换
-     * @param  array  $config       模板参数
-     * @param  array  $render       是否渲染内容
-     * @return [type]               [description]
+     * @param string $template 模板文件名
+     * @param array $vars 模板输出变量
+     * @param array $replace 模板替换
+     * @param array $config 模板参数
+     * @param bool $render 是否渲染内容
+     * @return mixed|void
+     * @throws \think\Exception
+     *  * widget模版输出
+     * @return [type]  [description]
      */
-    public function fetch($template='', $vars = [], $replace = [], $config = [] ,$render=false) {
-		$class      = get_class($this);
-		$names      = explode('\\', $class);
-		$module     = $names[1];
-		$controller = strtolower($names[3]);
+    public function fetch($template = '', $vars = [], $replace = [], $config = [], $render = false)
+    {
+        $class = get_class($this);
+        $names = explode('\\', $class);
+        $module = $names[1];
+        $controller = strtolower($names[3]);
 
         if ($template != '') {
-            $widget_view_path = $module_view_path = APP_PATH.$module.'/view/';
+            $widget_view_path = $module_view_path = APP_PATH . $module . '/view/';
             $template_path = '';
             if (MODULE_MARK === 'admin') {
                 $template_path = 'admin/';
-            } else{
+            } else {
                 $current_theme_path = THEME_PATH . CURRENT_THEME . '/'; //默认主题设为当前主题
-                $widget_view_path = $current_theme_path.$module.'/';
+                $widget_view_path = $current_theme_path . $module . '/';
             }
-            
+
             $template_path .= 'widget/';
 
-        	$template_path .= $controller.'/'.$template;
-            $template = $widget_view_path.$template_path. '.' .config('template.view_suffix');
-            
+            $template_path .= $controller . '/' . $template;
+            $template = $widget_view_path . $template_path . '.' . config('template.view_suffix');
+
             if (!is_file($template)) {
-                $template = $module_view_path.$template_path. '.' .config('template.view_suffix');
+                $template = $module_view_path . $template_path . '.' . config('template.view_suffix');
                 if (!is_file($template)) {
-                    throw new \Exception('模板不存在：'.$template, 5001);
+                    throw new \Exception('模板不存在：' . $template, 5001);
                 }
-                
+
             }
-            
-           echo $this->view->fetch($template, $vars, $replace, $config, $render);
+
+            echo $this->view->fetch($template, $vars, $replace, $config, $render);
         }
     }
 }
---
+++ apps/qlive/controller/Index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Editor: xpwsg
+ * Date: 2019/3/25
+ * Time: 14:19
+ * Dedicated to my wife and daughter
+ */
+
+namespace app\qlive\controller;
+
+
+use app\home\controller\Home;
+use think\Db;
+
+/**
+ * Class Index
+ * @package app\qlive\controller
+ * 直播模块首页
+ */
+class Index extends Home
+{
+    /**
+     *初始化
+     */
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+
+    public function index()
+    {
+        //正在直播的房间列表(从七牛获取)
+        $liveList = \logic('QliveLogic')->listLiveStream();
+        $liveList = $liveList['keys'];
+        $allLiveRoomInfo = [];  //所有在直播中的房间详情
+        //根据推流码查询直播间的详情
+        foreach ($liveList as $k => $v) {
+            $allLiveRoomInfo[$k]['room_info'] = Db::name('QliveRoomList')
+                ->where('stream', 'eq', $v)
+                ->field('stream,create_time,update_time', true)
+                ->find();
+        }
+        \halt($allLiveRoomInfo);
+        return $this->fetch();
+    }
+
+    public function test()
+    {
+
+    }
+}
