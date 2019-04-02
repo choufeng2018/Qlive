0f2fca5675c59c127ec368a1705205f622bd55c0
xpwsg <xpwsgg@163.com>
2019-04-02 09:30:21 +0800

    将退出登录移动到个人中心

--- apps/qlive/api/v1/Center.php
+++ apps/qlive/api/v1/Center.php
@@ -11,11 +11,38 @@ namespace app\qlive\api\v1;
 
 
 use app\rest\controller\RestUserBase;
+use think\Db;
 
+/**
+ * Class Center
+ * @package app\qlive\api\v1
+ * 个人中心的操作
+ */
 class Center extends RestUserBase
 {
+    /**
+     *个人中心首页
+     */
     public function index()
     {
 
+    }
+
+    /**
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     * 退出登录
+     */
+    public function logout()
+    {
+        $uid = \input('user_id');
+        Db::name('UserToken')
+            ->where([
+                'token' => $this->token,
+                'user_id' => $uid,
+                'device_type' => $this->deviceType,
+            ])
+            ->update(['token' => '']);
+        $this->success('退出成功');
     }
 }
