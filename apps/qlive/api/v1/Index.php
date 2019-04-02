0b13d2ea9f3de7433221ba81ea85dc1ee12e0ca6
xpwsg <xpwsgg@163.com>
2019-04-02 17:22:35 +0800

    首页接口完成

--- apps/qlive/api/v1/Index.php
+++ apps/qlive/api/v1/Index.php
@@ -12,11 +12,29 @@ namespace app\qlive\api\v1;
 
 use app\rest\controller\RestBase;
 
+/**
+ * Class Index
+ * @package app\qlive\api\v1
+ * 首页接口
+ */
 class Index extends RestBase
 {
     public function index()
     {
-        $key = \config('sign_key');
-        $this->success("Hello API",$key);
+        //顶部的预告
+        $list1 = \logic('AppriseLogic')->getAppriseList(1);
+        //强力推荐
+        $list2 = \logic('AppriseLogic')->getAppriseList(3);
+        //即将开播
+        $list3 = \logic('HistoryLogic')->getLiveHistory('>');
+        //往期热门
+        $list4 = \logic('HistoryLogic')->getLiveHistory('<');
+        $list = [
+            $list1,
+            $list2,
+            $list3,
+            $list4
+        ];
+        $this->success('OK', $list);
     }
 }
