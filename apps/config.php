24ac1e43358df1396cdb7b8c915fdbd89ad9a725
xpwsg <xpwsgg@163.com>
2019-04-03 15:40:08 +0800

    设置默认模块为admin; 并删除无需git版本控制的文件

--- apps/config.php
+++ apps/config.php
@@ -71,7 +71,7 @@ $config = [
     // +----------------------------------------------------------------------
 
     // 默认模块名
-    'default_module' => 'qlive',
+    'default_module' => 'admin',
     // 禁止访问模块
     'deny_module_list' => ['common'],
     // 默认控制器名
