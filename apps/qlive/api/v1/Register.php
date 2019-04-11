39673e59080c1c4931bb3dad94d4031a32f3a243
xpwsg <xpwsgg@163.com>
2019-04-11 16:31:29 +0800

    将通用代码分离

--- apps/qlive/api/v1/Register.php
+++ apps/qlive/api/v1/Register.php
@@ -10,10 +10,8 @@
 namespace app\qlive\api\v1;
 
 
-use app\common\logic\User as UserLogic;
 use app\common\model\User as UserModel;
 use app\rest\controller\RestBase;
-use think\Db;
 
 /**
  * Class Register
@@ -23,6 +21,7 @@ use think\Db;
  */
 class Register extends RestBase
 {
+    use CommonTrait;
 
     /**
      * @throws \think\Exception
@@ -55,76 +54,6 @@ class Register extends RestBase
             }
         } else {
             $this->error('提交方式不正确');
-        }
-    }
-
-    /**
-     * @param $username
-     * @throws \think\Exception
-     * 检查用户名是否可用,是否唯一
-     */
-    public function checkUserName($username)
-    {
-        //检测用户名是否被禁止注册
-        $check_username = UserLogic::checkDenyUser($username);
-        if ($check_username) {
-            $this->error('该用户名不可用');
-        }
-        //检查是否唯一
-        $userNameCount = Db::name('Users')
-            ->where('username', 'eq', $username)
-            ->count();
-        if ($userNameCount > 0) {
-            $this->error('该用户名已被占用');
-        }
-    }
-
-    /**
-     * @param $nickname
-     * @throws \think\Exception
-     * 检查昵称是否可用,是否唯一
-     */
-    public function checkNickName($nickname)
-    {
-        //检测昵称是否被禁止注册
-        $check_nickname = UserLogic::checkDenyUser($nickname);
-        if ($check_nickname) {
-            $this->error('该昵称不可用');
-        }
-        //检查是否唯一
-        $nickNameCount = Db::name('Users')
-            ->where('nickname', 'eq', $nickname)
-            ->count();
-        if ($nickNameCount > 0) {
-            $this->error('该昵称已被占用');
-        }
-    }
-
-    /**
-     * @param $email
-     * @throws \think\Exception
-     * 检查邮箱是否可用
-     */
-    public function checkEmail($email)
-    {
-        //检查是否唯一
-        $emailCount = Db::name('Users')
-            ->where('email', 'eq', $email)
-            ->count();
-        if ($emailCount > 0) {
-            $this->error('该邮箱已被占用');
-        }
-    }
-
-    /**
-     * @param $password
-     * @param $repassword
-     * 检查密码是否一致
-     */
-    public function checkPassword($password, $repassword)
-    {
-        if ($password !== $repassword) {
-            $this->error('两次密码不一致');
         }
     }
 }
