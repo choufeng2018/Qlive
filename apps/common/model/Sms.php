0178d6e91afa235493cc2575f163d74c580836b1
xpwsg <xpwsgg@163.com>
2019-04-12 11:37:15 +0800

    添加sms的model类

---
+++ apps/common/model/Sms.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Editor: xpwsg
+ * Date: 2019/4/12
+ * Time: 10:21
+ * Dedicated to my wife and daughter
+ */
+
+namespace app\common\model;
+
+
+class Sms extends Base
+{
+
+}
