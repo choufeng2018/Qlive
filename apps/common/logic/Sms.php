1fc1f87080de0b94ba55f4d2c2c23edfbdbd8c0e
xpwsg <xpwsgg@163.com>
2019-04-12 11:48:48 +0800

    验证注册验证码功能完成

---
+++ apps/common/logic/Sms.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Editor: xpwsg
+ * Date: 2019/4/12
+ * Time: 10:43
+ * Dedicated to my wife and daughter
+ */
+
+namespace app\common\logic;
+
+
+use think\Db;
+
+/**
+ * Class Sms
+ * @package app\common\logic
+ */
+class Sms extends Base
+{
+    /**
+     * @param $mobile
+     * @return array|false|\PDOStatement|string|\think\Model
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 根据手机号获取最近一条验证码的数据
+     */
+    public function getLastCodeInfoByMobile($mobile)
+    {
+        $code_info = Db::name('Sms')
+            ->where('mobile', 'eq', $mobile)
+            ->order('create_time desc')
+            ->find();
+        return $code_info;
+    }
+
+    /**
+     * @param $mobile
+     * @return bool
+     * @throws \think\Exception
+     * 检查发送频率和总条数
+     */
+    public function checkFrequency($mobile)
+    {
+        //一天最多十五条验证码
+        $count = Db::name('Sms')
+            ->where('mobile', 'eq', $mobile)
+            ->count();
+        //每次间隔不超过60s
+        $code_info = $this->getLastCodeInfoByMobile($mobile);
+        if (\time() - \strtotime($code_info['create_time']) < 60) {
+            return false;
+        } elseif ($count > 15) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    /**
+     * @param $mobile   手机号
+     * @param $code 验证码
+     * @param $type 类型
+     * @return bool
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 检查验证码是否正确或过期
+     */
+    public function checkCode($mobile, $code, $type)
+    {
+        $code_info = $this->getLastCodeInfoByMobile($mobile);
+        //检查验证码和过期时间
+        if ($code != $code_info['code']) {
+            return false;
+        } elseif (\time() - \strtotime($code_info['create_time']) > \config('alisms_config.expire')) {
+            return false;
+        } elseif ($type != $code_info['type']) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- apps/qlive/api/v1/Register.php
+++ apps/qlive/api/v1/Register.php
@@ -10,6 +10,7 @@
 namespace app\qlive\api\v1;
 
 
+use app\common\model\Sms;
 use app\common\model\User as UserModel;
 use app\rest\controller\RestBase;
 
@@ -51,6 +52,68 @@ class Register extends RestBase
                 $this->success('注册成功');
             } else {
                 $this->error('注册失败!');
+            }
+        } else {
+            $this->error('提交方式不正确');
+        }
+    }
+
+
+    /**
+     * @throws \think\Exception
+     * 获取注册手机验证码
+     */
+    public function getRegSms()
+    {
+        $mobile = \input('mobile');
+
+        if (empty($mobile)) {
+            $this->error('手机号不能为空');
+        }
+        $this->checkMobile($mobile);
+
+        $checkOne = \logic('Sms')->checkFrequency($mobile);
+        if (!$checkOne) {
+            $this->error('频率过快或已达当日限制');
+        }
+
+        $code = \rand_string(4, 1);
+        $param = [
+            'mobile' => $mobile,
+            'template' => \config('alisms_config.register'),
+            'templateParam' => [
+                'code' => $code,
+                'product' => '耳听美',
+            ],
+        ];
+        $res = \hook('sms', $param, true);
+        if ($res) {
+            //写入sms数据表中
+            $sql_data = [
+                'type' => 1,
+                'mobile' => $mobile,
+                'code' => $code,
+                'ip' => $this->request->ip(),
+            ];
+            Sms::create($sql_data);
+            $this->success('发送成功,验证码有效时间为' . \config('alisms_config.expire') . '秒');
+        } else {
+            $this->error('发送失败');
+        }
+    }
+
+    /**
+     *检测注册验证码
+     */
+    public function checkRegSms()
+    {
+        if ($this->request->isPost()) {
+            $param = \input();
+            $res = \logic('Sms')->checkCode($param['mobile'], $param['code'], 1);
+            if ($res) {
+                $this->success('验证通过');
+            } else {
+                $this->error('验证失败');
             }
         } else {
             $this->error('提交方式不正确');
