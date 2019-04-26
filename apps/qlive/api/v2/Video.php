1d0bdb8533a0022ebbdd16e98afeb7414d52ce60
xpwsg <xpwsgg@163.com>
2019-04-26 10:46:12 +0800

    V2版本的视频列表筛选

--- apps/qlive/api/v2/Video.php
+++ apps/qlive/api/v2/Video.php
@@ -24,7 +24,7 @@ class Video extends RestBase
      */
     public function index()
     {
-        $page = \input('page', 1,'intval');
+        $page = \input('page', 1, 'intval');
         $list = Db::name('QliveVideoList')
             ->where('status', 'eq', 1)
             ->page($page, 10)
@@ -49,7 +49,7 @@ class Video extends RestBase
      */
     public function detail()
     {
-        $video_id = \input('id','','intval');
+        $video_id = \input('id', '', 'intval');
         $video_info = Db::name('QliveVideoList')
             ->where('id', 'eq', $video_id)
             ->find();
@@ -64,6 +64,72 @@ class Video extends RestBase
             'video_info' => $video_info,
             'live_info' => $live_info,
         ];
+        $this->success('OK', $res);
+    }
+
+    /**
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 视频列表,带条件筛选
+     */
+    public function filter()
+    {
+        //筛选条件
+        $map = [];
+        //排序条件
+        $order = [];
+
+        $page = \input('page', 1);
+        //直播分类
+        if (\input('category')) {
+            $map['category'] = \input('category');
+        }
+        //直播类型
+        if (\input('type')) {
+            $map['type'] = \input('type');
+        }
+        //是否付费
+        if (\input('price')) {
+            $mark = \input('price') == 1 ? '>' : '=';
+            $map['price'] = [$mark, 0];
+        }
+        if (\input('order')) {
+            switch (\input('order')) {
+                case 1:
+                    $order = 'price asc';
+                    break;
+                case 2:
+                    $order = 'price desc';
+                    break;
+                case 3:
+                    $order = 'sales asc';
+                    break;
+                case 4:
+                    $order = 'sales desc';
+                    break;
+                case 5:
+                    $order = 'hits asc';
+                    break;
+                case 6:
+                    $order = 'hits desc';
+                    break;
+                default:
+                    $order = 'update_time desc';
+                    break;
+            }
+        }
+        $list = Db::name('QliveVideoList')
+            ->where($map)
+            ->order($order)
+            ->page($page, 10)
+            ->select();
+        $count = Db::name('QliveVideoList')
+            ->where($map)
+            ->count();
+        $res['count'] = $count;
+        $res['list'] = $list;
         $this->success('OK', $res);
     }
 }
