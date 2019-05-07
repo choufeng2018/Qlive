c671e76914d17d8a8a1afb1c5c612c4d497ea185
xpwsgg <xpwsgg@163.com>
2019-05-07 16:26:10 +0800

    定时发送直播预约短信通知

--- apps/qlive/controller/SmsApi.php
+++ apps/qlive/controller/SmsApi.php
@@ -10,12 +10,78 @@
 namespace app\qlive\controller;
 
 
+use app\common\model\Sms;
+use app\common\model\User;
+use app\qlive\model\QliveLiveHistory;
 use think\Controller;
+use think\Db;
 
+/**
+ * Class SmsApi
+ * @package app\qlive\controller
+ * 通知类短信的接口
+ */
 class SmsApi extends Controller
 {
+    /**
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 定时发送直播预约通知短信
+     */
     public function index()
     {
+        $list = Db::name('QliveAppoint')
+            ->whereTime('live_open_time', '30 minutes')
+            ->select();
+        foreach ($list as $v) {
+            $user_info = User::get($v['uid']);
+            $live_info = QliveLiveHistory::get($v['live_id']);
+            //短信发送记录入库数据
+            $sms_data = [
+                'type' => 3,
+                'mobile' => $user_info['mobile'],
+                'code' => '直播预约记录',
+                'ip' => $this->request->ip(),
+            ];
 
+            //发送短信
+            $templateCode = \config('alisms_config.live_appoint');
+            $param = [
+                'mobile' => $user_info['mobile'],
+                'template' => $templateCode,
+                'templateParam' => [
+                    'name' => $live_info['title'],
+                    'time' => $live_info['open_time'],
+                ],
+            ];
+            $sms_res = \hook('sms', $param, true);
+            if ($sms_res[0]) {
+                $res = Sms::create($sms_data);
+                if ($res) {
+                    $result = [
+                        'code' => 1,
+                        'msg' => '预约成功',
+                        'time' => \time(),
+                    ];
+                    return \json($result);
+                } else {
+                    $result = [
+                        'code' => 0,
+                        'msg' => '预约失败',
+                        'time' => \time(),
+                    ];
+                    return \json($result);
+                }
+            } else {
+                $result = [
+                    'code' => 0,
+                    'msg' => '短信发送失败',
+                    'time' => \time(),
+                ];
+                return \json($result);
+            }
+        }
     }
 }
