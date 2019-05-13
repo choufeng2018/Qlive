c02ce6c6d437ddbfeadb4a28c5b09146b35a5260
xpwsgg <xpwsgg@163.com>
2019-05-13 16:00:29 +0800

    V2首页接口完成

--- apps/qlive/api/v2/Index.php
+++ apps/qlive/api/v2/Index.php
@@ -20,14 +20,17 @@ use think\Db;
 class Index extends RestBase
 {
 
+    /**
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 首页数据
+     */
     public function index()
     {
-        //置顶项目：开播申请>正在直播>往期视频
-        $top = [
+        //置顶数据：开播申请>正在直播>往期视频
 
-        ];
-
-        //开播申请
+        //即将开播
         $top_apply = Db::name('QliveLiveHistory')
             ->whereTime('open_time', '>', \time())
             ->where('status', 'eq', '1')
@@ -65,20 +68,18 @@ class Index extends RestBase
                 'short_content' => $top_apply['description'],
                 'flag' => $top_apply['flag'],
             ];
-            $this->success('OK', $top);
         } elseif (empty($top_apply) && !empty($top_living)) {
             $top = [
-                'live_id' => $top_living['id'],
-                'title' => $top_living['title'],
-                'image' => \get_file_complete_path($top_living['logo']),
-                'lecturer' => $top_living['anchor'],
-                'category' => $top_living['category'],
-                'live_type' => $top_living['live_type'],
-                'start_time' => $top_living['open_time'],
-                'short_content' => $top_living['description'],
-                'flag' => $top_living['flag'],
+                'live_id' => $top_living[0]['id'],
+                'title' => $top_living[0]['title'],
+                'image' => \get_file_complete_path($top_living[0]['logo']),
+                'lecturer' => $top_living[0]['anchor'],
+                'category' => $top_living[0]['category'],
+                'live_type' => $top_living[0]['live_type'],
+                'start_time' => $top_living[0]['open_time'],
+                'short_content' => $top_living[0]['description'],
+                'flag' => $top_living[0]['flag'],
             ];
-            $this->success('OK', $top);
         } elseif (empty($top_apply) && empty($top_living)) {
             $top = [
                 'live_id' => $video['live_id'],
@@ -91,9 +92,57 @@ class Index extends RestBase
                 'short_content' => $video['description'],
                 'flag' => $video['flag'],
             ];
-            $this->success('OK', $top);
         } else {
-            $this->error('后台未指定数据', 'null');
+            $top = [];
         }
+
+        //近期开播数据
+        $immediate_list = Db::name('QliveLiveHistory')
+            ->whereTime('open_time', '>', \time())
+            ->where('status', 'eq', '1')
+            ->where('flag', 'neq', 2)
+            ->order('update_time desc')
+            ->field('id,title,open_time,logo,anchor,category,live_type,description,flag')
+            ->limit(3)
+            ->select();
+        if (!empty($immediate_list)) {
+            foreach ($immediate_list as $k => $v) {
+                $immediate_list[$k]['logo'] = \get_file_complete_path($v['logo']);
+            }
+        }
+
+        //往期热门
+        $hot_list = Db::name('QliveVideoList')
+            ->where('status', 'eq', 1)
+            ->order('hits desc')
+            ->field('id,title,anchor,live_id,category,logo,type,price,live_time,description')
+            ->limit(3)
+            ->select();
+        if (!empty($hot_list)) {
+            foreach ($hot_list as $k => $v) {
+                $hot_list[$k]['logo'] = \get_file_complete_path($v['logo']);
+            }
+        }
+
+        //强力推荐
+        $recommend_list = Db::name('QliveVideoList')
+            ->where('status', 'eq', 1)
+            ->where('is_recommend', 'eq', 1)
+            ->order('order,update_time desc')
+            ->field('id,title,anchor,live_id,category,logo,type,price,live_time,description')
+            ->limit(3)
+            ->select();
+        if (!empty($recommend_list)) {
+            foreach ($recommend_list as $k => $v) {
+                $recommend_list[$k]['logo'] = \get_file_complete_path($v['logo']);
+            }
+        }
+        $data = [
+            'top' => $top,
+            'immediate_list' => $immediate_list,
+            'hot_list' => $hot_list,
+            'recommend_list' => $recommend_list,
+        ];
+        $this->success('OK', $data);
     }
 }
