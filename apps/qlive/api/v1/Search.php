a414466d7daf4e36eb1bff205c2a0ee65487406b
xpwsgg <xpwsgg@163.com>
2019-05-23 11:58:00 +0800

    搜索结果增加区分，1=还未开播，2=正在直播，3=直播结束有视频

--- apps/qlive/api/v1/Search.php
+++ apps/qlive/api/v1/Search.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * Editor: xpwsg
- * Date: 2019/4/8
- * Time: 14:32
+ * Date: 2019/5/23
+ * Time: 10:58
  * Dedicated to my wife and daughter
  */
 
@@ -13,25 +13,19 @@ namespace app\qlive\api\v1;
 use app\rest\controller\RestBase;
 use think\Db;
 
-/**
- * Class Search
- * @package app\qlive\api\v1
- * 首页搜索
- */
 class Search extends RestBase
 {
-    /**
-     *
-     */
     public function index()
     {
         $keyword = \input('keyword');
         $page = \input('page', 1);
+
         $map = [];
         if ($keyword) {
             $map['title|anchor'] = ['like', '%' . $keyword . '%'];
             $map['open_time'] = ['> time', \time()];
         }
+        //未开播的列表
         //根据直播名称和主播搜索到的开播记录
         $live_list = \logic('HistoryLogic')->searchLiveHistory($map, $page);
 
@@ -39,9 +33,16 @@ class Search extends RestBase
         foreach ($live_list as $k => $v) {
             $live_res[$k]['id'] = $v['id'];
             $live_res[$k]['title'] = $v['title'];
-            $live_res[$k]['is_living'] = $v['is_living'];
+            $live_res[$k]['living_status'] = 1;
         }
-        //2019年5月16日添加：根据视频名称和主播搜索到的视频列表
+        //正在直播中的列表
+
+        $living_res = $this->getLivingRoomList($map);
+        if (empty($living_res)) {
+            $living_res = [];
+        }
+//        \halt($living_res);
+        //视频列表
         unset($map['open_time']);
         $video_list = Db::name('QliveVideoList')
             ->where($map)
@@ -52,28 +53,35 @@ class Search extends RestBase
         foreach ($video_list as $k => $v) {
             $video_res[$k]['id'] = $v['live_id'];
             $video_res[$k]['title'] = $v['title'];
-            $video_res[$k]['is_living'] = false;
+            $video_res[$k]['living_status'] = 3;
         }
-        $res = \array_merge($live_res, $video_res);
+        $res = \array_merge($live_res, $living_res, $video_res);
         $this->success('Ok', $res);
     }
 
-    public function index2()
+    function getLivingRoomList($map)
     {
-        $keyword = \input('keyword');
-        $map = [];
-        if ($keyword) {
-            $map['title|anchor'] = ['like', '%' . $keyword . '%'];
-        }
-        $list = Db::field('id,title')
-            ->name('QliveLiveHistory')
-            ->where($map)
-            ->union(function ($query) use ($map) {
-                $query->field('live_id,title')
+        unset($map['open_time']);
+        //从七牛获取到正在直播的stream
+        $livingStream = \logic('QliveLogic')->listLiveStream();
+        $livingStream = $livingStream['keys'];
+        //获取正在直播的房间id
+        if (!empty($livingStream)) {
+            $room_list = [];
+            foreach ($livingStream as $k => $value) {
+                $room_list[$k] = Db::name('QliveLiveHistory')
+                    ->alias('a')
+                    ->join('QliveRoomList b', 'a.room_id=b.id')
+                    ->where('b.stream', 'eq', $value)
+                    ->where('b.status', 'eq', 1)
                     ->where($map)
-                    ->name('QliveVideoList');
-            })
-            ->select();
-        $this->success('OK', $list);
+                    ->whereTime('a.open_time', 'd')
+                    ->field('a.id,a.title')
+                    ->order('a.open_time desc')
+                    ->find();
+                $room_list[$k]['living_status'] = 2;
+            }
+            return $room_list;
+        }
     }
 }
